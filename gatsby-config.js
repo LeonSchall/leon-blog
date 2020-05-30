10e3908e7571af4b84e5b02568597a295a0f34f5
LeonSchall <66140509+LeonSchall@users.noreply.github.com>
2020-05-30 11:25:38 +0200

    Update Titel und Autorbeschreibung

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Test Name`,
+    title: `Leon's Blog!`,
     author: {
       name: `Leon Schall`,
-      summary: `who doesn't live and work in San Francisco building useful things.`
+      summary: `who lives and works in Vossenack building useful things.`
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
